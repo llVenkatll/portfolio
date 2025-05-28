368a418db2c9a7a3b414b8826ea7280fc6379166
Venkatesh Meenakshinathan <153883789+llVenkatll@users.noreply.github.com>
2025-05-28 23:23:24 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
               <FaGithub size={24} />
             </a>
             <a 
-              href="https://www.linkedin.com/in/venkatesh-meenakshinathan-a60a88249/" 
+              href="https://www.linkedin.com/in/venkateshmeenakshinathan/" 
               target="_blank" 
               rel="noopener noreferrer"
               className="text-secondary-theme hover:text-accent-theme transition-colors duration-300"
